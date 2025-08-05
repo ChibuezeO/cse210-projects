ac795321388b0b2a6bce7b4d6bd5a42e3a1d4992
ChibuezeO <hyacinth.okoro@yahoo.com>
2025-08-05 14:39:25 +0100

    week05 homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Gordon B. Hinckley", "Teachings of The Presidents Of The church");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("James E. Talmage", "Jesus The Christ", "Lord of The Sabbath");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
